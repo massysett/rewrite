d0508e98e0720af5fa4b94138c6b2a6f9e894923
Omari Norman <omari@smileystation.com>
2013-07-09 07:38:55 -0400

    add program

---
+++ rewrite.hs
@@ -0,0 +1,120 @@
+module Main (main) where
+
+import Data.Either (partitionEithers)
+import qualified System.Console.MultiArg as MA
+import qualified System.IO as IO
+import System.IO.Temp (withSystemTempDirectory)
+import qualified System.Exit as Exit
+import qualified System.Directory as D
+import qualified System.Process as P
+
+help :: String -> String
+help pn = unlines
+  [ "usage: " ++ pn ++ "[options] FILE PROGRAM [program options]"
+  , "opens given FILE and feeds it to standard input of PROGRAM"
+  , "and writes PROGRAM output back to FILE."
+  , ""
+  , "Aborts if PROGRAM exits with non-zero exit status."
+  , ""
+  , "Options:"
+  , "  -h, --help          Show help and exit"
+  , "  -b, --backup SUFFIX Back up FILE to file with given SUFFIX"
+  , "                      before writing new file"
+  ]
+
+type Backup = String
+type ProgramOpt = String
+type ProgName = String
+type InputFile = String
+
+errExit :: String -> IO a
+errExit msg = do
+  pn <- MA.getProgName
+  IO.hPutStrLn IO.stderr $ pn ++ ": error: " ++ msg
+  Exit.exitFailure
+
+-- | Parses command line arguments; returns whether to do backup and
+-- positional arguments
+parseArgs :: IO (Maybe Backup, InputFile, ProgName, [ProgramOpt])
+parseArgs = do
+  let opts = [ MA.OptSpec ["backup"] "b" (MA.OneArg Left) ]
+  as <- MA.simpleWithHelp help MA.StopOptions opts (return . Right)
+  let (baks, os) = partitionEithers as
+  bak <- case baks of
+    [] -> return Nothing
+    x:[] -> if null x
+            then errExit "empty backup suffix given"
+            else return $ Just x
+    _ -> errExit "multiple backup suffixes given"
+  (input, name, progOpts) <- case os of
+    [] -> errExit "no input file or program name given"
+    _:[] -> errExit "no program name given"
+    x:y:xs -> return (x, y, xs)
+  return (bak, input, name, progOpts)
+
+
+doBackup :: InputFile -> Backup -> IO ()
+doBackup inf bak = D.copyFile inf (inf ++ "." ++ bak)
+
+
+-- | Gets input from the file. Returns a computation that, when
+-- applied to the name of a temporary directory, will complete the writing
+-- of the program output to disk.
+getInput
+  :: Maybe Backup
+
+  -> InputFile
+  -- ^ Name of input file
+
+  -> ProgName
+
+  -> [ProgramOpt]
+
+  -> IO.Handle
+  -- ^ Handle to input file
+
+  -> FilePath
+  -- ^ Temporary directory
+
+  -> IO ()
+getInput mayBak inFile pn opts inHandle tempPath =
+  let outPath = tempPath ++ "/output"
+  in IO.withFile outPath IO.WriteMode $ \outHandle -> do
+    writeReadme tempPath
+    let cp = P.CreateProcess
+          { P.cmdspec = P.RawCommand pn opts
+          , P.cwd = Nothing
+          , P.env = Nothing
+          , P.std_in = P.UseHandle inHandle
+          , P.std_out = P.UseHandle outHandle
+          , P.std_err = P.Inherit
+          , P.close_fds = False
+          , P.create_group = False }
+    (_, _, _, procHndle) <- P.createProcess cp
+    code <- P.waitForProcess procHndle
+    _ <- case code of
+      Exit.ExitSuccess -> return ()
+      Exit.ExitFailure bad ->
+        errExit $ "program " ++ pn ++ " exited with code "
+                  ++ show bad
+    _ <- case mayBak of
+      Nothing -> return ()
+      Just bak -> doBackup inFile bak
+    D.copyFile outPath inFile
+
+
+writeReadme
+  :: FilePath
+  -- ^ Temporary directory
+  -> IO ()
+
+writeReadme fp = writeFile (fp ++ "/README")
+  "This directory created by the rewrite program."
+
+main :: IO ()
+main = do
+  (mayBak, inf, pn, opts) <- parseArgs
+  let getIn h = return $ getInput mayBak inf pn opts h
+  applyToTempPath <- IO.withFile inf IO.ReadMode getIn
+  withSystemTempDirectory "rewrite" applyToTempPath
+  
